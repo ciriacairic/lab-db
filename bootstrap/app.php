0be8a9e33b86b6c782e4f65025514853a4688656
Lucca Oliveira <lucca.oliveira@itigo.com.br>
2025-06-27 08:13:13 -0300

    Removendo cors dnv

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->append(\App\Http\Middleware\removeCORS::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
